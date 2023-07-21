6f788c689669a0cb7d66085a4976596daf4f3464
Xavier Mas <xvrmasgarcia@gmail.com>
2023-07-21 12:37:10 +0200

    commit

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: xamas-ga <xamas-ga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 12:59:18 by xamas-ga          #+#    #+#             */
-/*   Updated: 2023/07/20 19:06:30 by xamas-ga         ###   ########.fr       */
+/*   Updated: 2023/07/21 12:35:20 by xavier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
@@ -126,4 +126,4 @@ int	ft_printf(char const *format, ...)
 	ft_printf("Format  %%i: %i \n", num);
 
 	return (0);
-}*
+}*/
